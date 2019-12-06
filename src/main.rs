59976b85a4ffbddf7288853d85043842650b39c1
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-06 20:39:41 +0900

    feat: define AST

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+enum Expr {
+    Var (String),
+    Lam (String, Box<Expr>),
+    App (Box<Expr>, Box<Expr>),
+}
+
+fn reduce(expr: Expr) -> Expr {
+    match expr {
+        Expr::Var(x) => Expr::Var(x),
+        Expr::Lam(x, e) => Expr::Lam(x, e),
+        Expr::App(e0, e1) => Expr::App(e0, e1),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
