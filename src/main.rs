5f2bba12401436116d2896bd196753684013cbc3
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-07 14:44:40 +0900

    feat: free

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+
+#[derive(Clone, Debug)]
 enum Expr {
     Var (String),
     Lam (String, Box<Expr>),
@@ -5,15 +8,61 @@ enum Expr {
 }
 use Expr::{Var, Lam, App};
 
-fn reduce(expr: Expr) -> Box<Expr> {
-    match expr {
-        Var(x) => Box::new(Var(x)),
-        Lam(x, e) => Box::new(Lam(x, e)),
-        App(e0, e1) => match e0 {
-            Var(x) => Box::new(App(Box::new(Var(x)), reduce(e1)))
+impl Expr {
+    fn reduce(&self) -> Expr {
+        match self {
+            Var(_) => self.clone(),
+            Lam(x, e) => Lam(x.to_string(), Box::new(e.reduce())),
+            App(e0, e1) => {
+                let e0 = e0.reduce();
+                let e1 = e1.reduce();
+                match e0 {
+                    Var(_) => App(Box::new(e0), Box::new(e1)),
+                    Lam(_, _) => App(Box::new(e0), Box::new(e1)),
+                    App(_, _) => App(Box::new(e0), Box::new(e1)),
+                }
+            }
         }
     }
+
+    fn subst(&self, x:String, y:Expr) -> Expr {
+        match self {
+            Var(n) => if x == n.to_string() { self.clone() } else { y },
+            Lam(n, z) => {
+                if x == n.to_string() {
+                    y.clone()
+                } else {
+                    y
+                }
+            },
+            App(e0, e1) => App(Box::new(e0.subst(x.to_string(), y.clone())), Box::new(e1.subst(x.to_string(), y.clone()))),
+        }
+    }
+
+    fn free(&self) -> HashSet<String> {
+        match self {
+            Var(x) => {
+                let mut ns = HashSet::new();
+                ns.insert(x.to_string());
+                ns
+            },
+            Lam(n, e) => {
+                let mut ns = e.free();
+                ns.remove(&n.to_string());
+                ns
+            }
+            App(e0, e1) => {
+                let e2 = e0.free();
+                let e3 = e1.free();
+                e2.union(&e3);
+                e2
+            }
+        }
+    }
+
 }
+
+
 
 fn main() {
     println!("Hello, world!");
